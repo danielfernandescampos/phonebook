05e311fa65b7b8779d0b9d3a55a95756aebdbc68
Daniel <daniel.campos@picpay.com>
2022-05-19 13:01:23 -0300

    feat: connect to mongo db

--- index.js
+++ index.js
@@ -1,91 +1,120 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-var morgan = require('morgan');
+var morgan = require("morgan");
+// const mongoose = require("mongoose");
+const Person = require("./models/person");
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms :body'));
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(morgan(":method :url :status :response-time ms :body"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Phonebook</h1>");
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => String(person.id) === id);
-  if (!person)
-    return response.status(404).json({ error: "cannot find person" });
-  return response.json(person);
-});
-
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, error) => {
   const time = new Date();
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${time}</p>
-  `);
+  Person.find({})
+    .then(persons => {
+      response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${time}</p>
+      `);
+    })
+    .catch(error => next(error))
 });
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  const newPerson = {...body, id: generateId()}
-  if (!body.name)
-    return response.status(400).json({ error: "name was not found" });
-  if (!body.number)
-    return response.status(400).json({ error: "number was not found" });
-  if (persons.find(person => person.name === body.name))
-    return response.status(400).json({ error: "name is already registered" });
-  if (persons.find(person => person.number === body.number))
-    return response.status(400).json({ error: "number is already registered" });
-  persons.push(newPerson);
-  response.json(newPerson);
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+  .then((persons) => {
+    console.log("phonebook:");
+    response.json(persons);
+    // mongoose.connection.close();
+  })
+  .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const index = persons.findIndex(person => String(person.id) === id);
-  if(index < 0) return response.status(404).json({ error: "person not found" });
-  persons.splice(index,1);
-  response.status(204).end()
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person)
+      return response.status(404).json({ error: "Person not found" });
+      response.json(person)
+    })
+    .catch(error => next(error))
+});
+
+app.post("/api/persons", (request, response, next) => {
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number,
+  });
+
+  if (!person.name || !person.number)
+    return response.status(400).json({ error: "Name and number are required" });
+
+  Person.findOne({name: person.name}).then(person => {
+    if(person) return response.status(400).json({error: "This name is already registered"})
+  })
+  
+  Person.findOne({number: person.number}).then(person => {
+    if(person) return response.status(400).json({error: "This number is already registered"})
+  })
+
+  person.save().then((newPerson) => {
+    response.json(newPerson);
+    // mongoose.connection.close();
+  });
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+
+// MIDLEWARES 
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const PORT = process.env.PORT || 3002
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(`${error.name}: ${error.message}`)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: `${error.message}` })
+  } 
+  if (error.name === 'ObjectParameterError') {
+    return response.status(400).send({ error: `${error.message}` })
+  } 
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
